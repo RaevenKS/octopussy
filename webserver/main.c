e6b01edc9f23a13999af5f39231ba81653849d0f
Axel MONNIER <axel.monnier@etudiant.univ-lille1.fr>
2015-10-13 18:15:23 +0200

    GET

--- webserver/main.c
+++ webserver/main.c
@@ -33,11 +33,16 @@ void initialiser_signaux(){
 int main(){
   
   
-  const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n Quae circumspecta exsertare crispare edita coeperint et edita observatum in terna crispare nec bina in est ob quod exsertare circumspecta ob rem ob in contingit milites nec nec nec quod pecudum adsidue observatum trucidantur nec trucidantur grassatores bina cedunt exsertare bina edita edita et milites coeperint cedunt cedunt observatum pecudum nec terna contingit et ob vel exsertare quae iniquitati deinceps ritu trucidantur vehunt trucidantur potuerint in cum iniquitati ubi loci ob iniquitati iniquitati observatum circumspecta nec crispare potuerint quod montium permissi quae tela pecudum edita crispare potuerint in vel trucidantur ritu montium bina crispare rem montium cedunt est cedunt exsertare.\n";
-  int socket_serveur = creer_serveur(8081);
+  const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
+  int socket_serveur = creer_serveur(8080);
   int socket_client;
   char buffer[80];
   FILE * file;
+  char *str = "poule";
+  char *token;
+  int ok = 1;
+  int tokcpt = 0;
+  int lignevide = 0;
   
   initialiser_signaux();
   while(1){
@@ -52,14 +57,40 @@ int main(){
       file = fdopen(socket_client, "w+");
       if(file == NULL)
 	perror("fdopen");
-      fprintf(file, "<Octopussy> %s", message_bienvenue);
-      fflush(file);
-      while(1){
-	fgets(buffer, 80, file);
-	fflush(file);
-	fprintf(file, "<Octopussy> %s", buffer);
+      str = fgets(buffer, 80, file);
+      token = strtok(str, " ");
+      while(token)
+	{
+	  tokcpt++;
+	  if(tokcpt==1 && strcmp(token, "GET")!=0){
+	      ok = 0;
+	    }
+	  if(tokcpt >3){
+	      ok = 0;
+	    }
+	  if(tokcpt==3 && strcmp(token, "HTTP/1.0\r\n")!=0 && (strcmp(token, "HTTP/1.1\r\n")!=0)){
+	    ok = 0;
+	  }
+	  token=strtok(NULL, " "); 
+	}
+      if(tokcpt < 3){
+	  ok = 0;
+	}
+      while(lignevide==0)
+	{
+	  str = fgets(buffer, 80, file);
+	  if(str!=NULL && (strcmp(str, "\r\n")==0 || strcmp(str, "\n")==0))
+	    lignevide = 1;
+	}
+      if(ok==1){
+	fprintf(file, "<Octopussy> %s\n HTTP/1.1 200 OK\r\nConnection: open\r\nContent-length:%d\r\n\n200 OK\n", message_bienvenue, (int)strlen(message_bienvenue));
 	fflush(file);
       }
+      else{
+	fprintf(file, "<Octopussy>HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-length:17\r\n\n400 Bad Request\n ");
+	fflush(file);
+      }
+      exit(0);
     }
     else{
       close(socket_client);
